3ef7c2dc1e5f9d384988775cf7e34131622c4816
mltsvektr <mltsvekaterina@yandex.ru>
2023-10-03 23:07:37 +0300

    created generateNames, generateRandomNumber

--- src/main/java/ru/maltseva/stateapp/_main/_Main.java
+++ src/main/java/ru/maltseva/stateapp/_main/_Main.java
@@ -2,22 +2,26 @@ package ru.maltseva.stateapp._main;
 
 import ru.maltseva.stateapp.model.Citizen;
 import ru.maltseva.stateapp.model.Region;
+import ru.maltseva.stateapp.model.services.CitizenService;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class _Main {
     public static void main(String[] args) {
-        Citizen firstCit = new Citizen();
-        Citizen firstCit1 = new Citizen();
-        Citizen firstCit2 = new Citizen();
-        Citizen firstCit3 = new Citizen();
-        Citizen firstCit4 = new Citizen();
-        System.out.println(firstCit.getId());
-        System.out.println(firstCit1.getId());
-        System.out.println(firstCit2.getId());
-        System.out.println(firstCit3.getId());
-        System.out.println(firstCit4.getId());
-        
-    }
-}
+//        Citizen firstCit = new Citizen();
+//        Citizen firstCit1 = new Citizen();
+//        Citizen firstCit2 = new Citizen();
+//        Citizen firstCit3 = new Citizen();
+//        Citizen firstCit4 = new Citizen();
+//        System.out.println(firstCit.getId());
+//        System.out.println(firstCit1.getId());
+//        System.out.println(firstCit2.getId());
+//        System.out.println(firstCit3.getId());
+//        System.out.println(firstCit4.getId());
+        System.out.println(CitizenService.generateNames() + " " + CitizenService.generateNames());
+            }
+        }
+
+
 
--- src/main/java/ru/maltseva/stateapp/model/services/CitizenService.java
+++ src/main/java/ru/maltseva/stateapp/model/services/CitizenService.java
@@ -1,4 +1,27 @@
 package ru.maltseva.stateapp.model.services;
 
+import java.util.Random;
+
 public class CitizenService {
+    public static String generateNames() {
+        char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u', 'y'};
+        char[] consonants = new char[]{'p', 'b', 'k', 'f', 'v', 'm', 'z', 'h', 't', 'd', 'l', 'n'};
+        StringBuilder wholeName = new StringBuilder();
+        int result = CitizenService.generateRandomNumber(5, 10);
+        for (int i = 0; i < result; i++) {
+            if (i % 2 == 0) {
+                int index = CitizenService.generateRandomNumber(0, vowels.length);
+                wholeName.append(vowels[index]);
+            } else {
+                int index = CitizenService.generateRandomNumber(0, consonants.length);
+                wholeName.append(consonants[index]);
+            }
+        }
+        return wholeName.toString();
+    }
+
+    public static int generateRandomNumber(int low, int high) {
+        Random random = new Random();
+        return random.nextInt(high - low) + low;
+    }
 }
